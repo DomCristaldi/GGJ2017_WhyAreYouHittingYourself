55ff1edf336c60ecfbe345060aa81a656ae14d65
Unknown <andy.denost.li@gmail.com>
2017-01-21 12:00:42 -0500

    update Damage and Stats

--- WhyAreYouHittingYouself/Assets/Scripts/Stats.cs
+++ WhyAreYouHittingYouself/Assets/Scripts/Stats.cs
@@ -4,18 +4,47 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour {
 
-	public float health;
-
-	public float maxHealth = 12.0f;
-
-
 	// Use this for initialization
+	public int brainPower;
+	public int maxPower;
+	public int minPower;
+	public int healthState;
+	public int maxHealth;
+	public int minHealth;
+	public int damagePoint;
+	public int brainWaveRadius;
+	public bool death;
+
 	void Start () {
 
 	}
-	
-	// Update is called once per frame
 	void Update () {
-
+		//damagePoint = 
 	}
+
+	void OnTriggerEnter(Collider other){
+		Damage otherDamageScript = other.gameObject.GetComponent<Damage>();
+		if(otherDamageScript!=null){
+			healthState-=otherDamageScript.damageAmount;
+		}
+	}
+
+	void Health(){
+		healthState=healthState-damagePoint;
+		if (healthState>maxHealth){
+			healthState=maxHealth;
+		}else if(healthState<=minHealth){
+			healthState=minHealth;
+			death=true;
+			Destroy(gameObject);
+		}
+	}
+	void Power(){
+		if (brainPower>maxPower){
+			brainPower=maxPower;
+		}else if(brainPower<minPower){
+			brainPower=minPower;
+		}
+	}
+
 }
