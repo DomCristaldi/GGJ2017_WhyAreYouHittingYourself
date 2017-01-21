dfb672646739002b1d70129925edde0882c67a74
Unknown <xiuman893821@gmail.com>
2017-01-21 00:28:11 -0500

    movementcontroller

---
+++ WhyAreYouHittingYouself/Assets/Scripts/movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+    public float speed;
+    public Vector3 trueMoveDirection;
+    public Vector3 desireMoveDirection;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        trueMoveDirection = trueMoveDirection.normalized;
+        desireMoveDirection = desireMoveDirection.normalized;
+
+        transform.position = new Vector3
+        (
+            transform.position.x+(speed*Time.deltaTime*trueMoveDirection.x),
+            0.0f,
+            transform.position.z+(speed*Time.deltaTime*trueMoveDirection.z)
+        );
+        trueMoveDirection = desireMoveDirection;
+
+	}
+}
